b523a5a4c9dbf5f926604886a22421f9eb61fd88
rtougn <rtougn@users.noreply.github.com>
2019-01-10 17:55:18 +0700

    upload 2
    2

---
+++ ajax/ajax.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {  
+    loadtien();
+    loadtui();
+    var explode = function(){
+                  $("#kq").text("");
+                };
+    function loadtien(){
+        $.ajax({
+            url: 'php/tien.php',
+            type: "POST",
+            data: ({}),
+            success: function(data){
+                $("#tien").html(data);
+            }
+        });
+    }
+    function loadtui(){
+        $.ajax({
+            url: 'php/tui.php',
+            type: "POST",
+            data: ({}),
+            success: function(data){
+                $("#tui").html(data);
+            }
+        });
+    }
+    $("#btn").click(function(){ 
+        $.ajax({
+            url: 'php/mua.php',
+            type: "POST",
+            data: ({ten: $('#txtten').val(),soluong: $('#sl').val()}),
+            success: function(data){
+                $("#kq").text(data);
+                loadtien();
+                loadtui();
+                setTimeout(explode, 3000);
+            }
+        });        
+    });
+});
